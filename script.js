20a93c6c1abfd402e5037c9fb513a3bd157f21a4
Daniel <danielyoussef1@yahoo.com>
2024-07-18 23:25:20 -0700

    finished playRound function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let randomNumber = Math.random();
     console.log(randomNumber);
@@ -18,6 +15,36 @@ function getHumanChoice() {
     return input;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+    let humanScore = 0;
+    let computerScore = 0;
+
+    if (humanChoice == "rock" && computerChoice == "Scissors") {
+        console.log("You win! Rock beats Paper!");
+        humanScore++;
+    } else if (humanChoice == "paper" && computerChoice == "Rock") {
+        console.log("You win! Paper beats Rock!");
+        humanScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "Paper") {
+        console.log("You win! Scissors beats Paper!");
+        humanScore++;
+    } else if (computerChoice == "Rock" && humanChoice == "scissors") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore++;
+    } else if (computerChoice == "Paper" && humanChoice == "rock") {
+        console.log("You lose! Paper beats Rock!");
+        computerScore++;
+    } else if (computerChoice == "Scissors" && humanChoice == "paper") {
+        console.log("You lose! Scissors beats Paper!");
+        computerScore++;
+    } else {
+        console.log("Draw!")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
