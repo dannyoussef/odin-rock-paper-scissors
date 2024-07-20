41726a3a7ef1b073e8ad89e4baede9d835bf19d6
Daniel <danielyoussef1@yahoo.com>
2024-07-19 17:22:28 -0700

    added selection buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
   let randomNumber = Math.random();
-  console.log(randomNumber);
+
   if (randomNumber <= 0.3) {
     return "Rock";
   } else if (randomNumber > 0.3 && randomNumber <= 0.6) {
@@ -19,7 +19,7 @@ function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
 
   if (humanChoice == "rock" && computerChoice == "Scissors") {
-    console.log("You win! Rock beats Paper!");
+    console.log("You win! Rock beats Scissors!");
     return 1;
   } else if (humanChoice == "paper" && computerChoice == "Rock") {
     console.log("You win! Paper beats Rock!");
@@ -41,23 +41,46 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+// function playGame() {
+//   let humanScore = 0;
+//   let computerScore = 0;
 
-  for (i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    if (playRound(humanSelection, computerSelection) == 1) {
-      humanScore++;
-    }
-    if (playRound(humanSelection, computerSelection) == 0) {
-      computerScore++;
-    }
-  }
+// for (i = 0; i < 5; i++) {
+//   const humanSelection = getHumanChoice();
+//   const computerSelection = getComputerChoice();
+//   if (playRound(humanSelection, computerSelection) == 1) {
+//     humanScore++;
+//   }
+//   if (playRound(humanSelection, computerSelection) == 0) {
+//     computerScore++;
+//   }
+// }
 
-  console.log("\nYour Score:\t ", humanScore);
-  console.log("Computer Score:\t ", computerScore);
-}
+//   console.log("\nYour Score:\t ", humanScore);
+//   console.log("Computer Score:\t ", computerScore);
+// }
 
-playGame();
+const rockBtn = document.querySelector("#rock-btn");
+rockBtn.addEventListener("click", function (rockSelected) {
+  playRound("Rock", getComputerChoice());
+});
+
+const paperBtn = document.querySelector("#paper-btn");
+paperBtn.addEventListener("click", function (paperSelected) {
+  playRound("Paper", getComputerChoice());
+});
+
+const scissorsBtn = document.querySelector("#scissors-btn");
+scissorsBtn.addEventListener("click", function (scissorsSelected) {
+  playRound("Scissors", getComputerChoice());
+});
+
+// const container = document.querySelector("#container");
+
+// const results = document.createElement("div");
+// results.classList.add("results");
+// results.textContent = "Results";
+
+// container.appendChild(results);
+
+// playGame();
